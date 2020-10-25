66ad58c35501bcc377daa5122edfb2e57c448e3a
Vansh <vansharora2929@gmail.com>
2020-10-25 18:06:53 +0530

    Added checks

--- Queue.cpp
+++ Queue.cpp
@@ -4,3 +4,19 @@ int queue[10];
 int n = 10;
 int front = -1;
 int rear = -1;
+
+int isEmpty()
+{
+    if(rear == front)
+        return 1;
+    else 
+        return 0;
+}
+
+int isFull()
+{
+    if(front == n-1)
+        return 1;
+    else
+        return 0;
+}
