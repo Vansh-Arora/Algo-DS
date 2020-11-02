19a49d23defb0b03846ba2dfa2f155c5a85e6a42
Vansh <vansharora2929@gmail.com>
2020-11-02 09:48:01 +0530

    Added a method to get top element without removing it

--- Stack/Stack.c
+++ Stack/Stack.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<limits.h>
 int stack[10];
 int n = 10;
 int top = -1;
@@ -17,7 +17,7 @@ int isFull()
 void display()
 {
     if(isEmpty())
-        printf("Stack is empty.\n");
+        printf("Stack empty.\n");
     else
     {
         for(int i=0; i<=top; i++)
@@ -28,20 +28,30 @@ void display()
 void push(int data)
 {
     if(isFull())
-        printf("Stack is full, can't insert.");
+        printf("Stack Overflow.\n");
     else
         stack[++top] = data;
 }
 
-void pop()
+int pop()
 {
     if(isEmpty())
-        printf("Stack underflow.");
+    {
+        printf("Stack underflow.\n");
+        return INT_MIN;
+    }
     else
-        printf("Popped element %d\n",stack[top--]);
+        return(stack[top--]);
 }
 
-//int top()
+int getTop()
+{
+    if(isEmpty()){
+        printf("Stack empty.\n");
+        return INT_MIN;}
+    else
+        return top;
+}
 //int size()
 
 int main()
@@ -53,6 +63,7 @@ int main()
         printf("\n1: Press 1 to push.\n");
         printf("2: Press 2 to pop.\n");
         printf("3: Press 3 to display stack.\n");
+        printf("4: Press 4 to get top.\n");
         printf("0: Press 0 to exit.\n");
         printf("Enter your choice: ");
 
@@ -66,18 +77,23 @@ int main()
                 break;
 
             case 2:
-                pop();
+                printf("Popped element: %d\n",pop());                   
                 break;
 
             case 3:
                 display();
                 break;
 
+            case 4:
+                if(!isEmpty())
+                    printf("Top: %d",getTop());
+                break;
+
             case 0:
                 return 0;
                 
             default:
-                printf("Invalid input!");
+                printf("Invalid input!\n");
         }
     }   
 }
