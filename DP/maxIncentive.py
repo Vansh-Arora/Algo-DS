1498a68d99309b869910d4436701691522d760b8
Vansh <vansharora2929@gmail.com>
2021-03-07 05:03:16 +0530

    commented

--- DP/maxIncentive.py
+++ DP/maxIncentive.py
@@ -2,13 +2,17 @@ n = int(input())
 values = input().split()
 values = [int(i) for i in values]
 V = []
+# Making an n*n matrix
 for i in range(n+1):
-    V.append([])
-    for j in range(n+1):
-        if i == 0 or j == 0:
+    V.append([])     # adding n rows
+    for j in range(n+1):  # in columns
+        if i == 0 or j == 0:   # making 1st row and column 0
             V[i].append(0)
+
+        # following condition is for cases only when number of elements
+        # to be considered is less than number of elements in current list
         elif j<=i:
-            vals = values[i-j:i]
+            vals = values[i-j:i]    # making a list of size j, upwards from current index    
             temp = max(sum(vals) * min(vals), V[i-1][j])
             V[i].append(max(V[i-1][j],V[i][j-1],temp))
         else:
